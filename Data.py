012d676adc8812dc31e120be9c70c00a82457bf4
NatWalsh2709 <137919826+NatWalsh2709@users.noreply.github.com>
2023-06-27 09:32:56 -0700

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,78 @@
+
+import names
+import pandas as pd
+import numpy as np
+import base64
+import io
+
+
+'''
+Generate random guests list
+:parameter
+    :param n: num - number of guests and length of dtf
+    :param lst_categories: list - ["family", "friends", "university", ...]
+    :param n_rules: num - number of restrictions to apply (ex. if 1 then 2 guests can't be sit together)
+:return
+    dtf with guests
+'''
+def random_data(n=100, lst_categories=["family","friends","work","university","tennis"], n_rules=0):
+    ## basic list
+    lst_dics = []
+    for i in range(n):
+        name = names.get_full_name()
+        category = np.random.choice(lst_categories) if len(lst_categories) > 0 else np.nan
+        lst_dics.append({"id":i, "name":name, "category":category, "avoid":np.nan})
+    dtf = pd.DataFrame(lst_dics)
+
+    ## add rules
+    if n_rules > 0:
+        for i in range(n_rules):
+            choices = dtf[dtf["avoid"].isna()]["id"]
+            ids = np.random.choice(choices, size=2)
+            dtf["avoid"].iloc[ids[0]] = int(ids[1]) if int(ids[1]) != ids[0] else int(ids[1])+1
+
+    return dtf
+
+
+'''
+When a file is uploaded it contains "contents", "filename", "date"
+:parameter
+    :param contents: file
+    :param filename: str
+:return
+    pandas table
+'''
+def upload_file(contents, filename):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            return pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            return pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print("ERROR:", e)
+        return 'There was an error processing this file.'
+
+
+'''
+Write excel
+:parameter
+    :param dtf: pandas table
+:return
+    link
+'''
+def download_file(dtf):
+    xlsx_io = io.BytesIO()
+    writer = pd.ExcelWriter(xlsx_io)
+    dtf.to_excel(writer, index=False)
+    writer.save()
+    xlsx_io.seek(0)
+    media_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    data = base64.b64encode(xlsx_io.read()).decode("utf-8")
+    link = f'data:{media_type};base64,{data}'
+    return link 
+    
+
+
+
---
+++ Plot.py
@@ -0,0 +1,52 @@
+
+import plotly.express as px
+import numpy as np
+import pandas as pd
+
+
+class Plot():
+
+    def __init__(self, dtf):
+       self.dtf = self.prepare_data(dtf)
+
+
+    @staticmethod
+    def prepare_data(dtf):
+        ## mark the rules
+        dtf["avoid"] = dtf["avoid"].apply(lambda x: dtf[dtf["id"]==x]["name"].iloc[0] if pd.notnull(x) else "none")
+        dtf["size"] = dtf["avoid"].apply(lambda x: 1 if x == "none" else 3)
+
+        ## create axis
+        dtf_out = pd.DataFrame()
+        lst_tables = []
+        for t in dtf["table"].unique():
+            dtf_t = dtf[dtf["table"]==t]
+            n = len(dtf_t)
+            theta = np.linspace(0, 2*np.pi, n)
+            dtf_t["x"] = 1*np.cos(theta)
+            dtf_t["y"] = 1*np.sin(theta)
+            dtf_out = dtf_out.append(dtf_t)
+
+        return dtf_out.reset_index(drop=True).sort_values("table")
+
+
+    def print_title(self, max_capacity, filename=None):
+        guests = str(int(len(self.dtf)))
+        tables = str(int(len(self.dtf["table"].unique())))
+        process = "Random Simulation" if filename is None else "Data from "+filename
+        max_capacity = str(int(max_capacity))
+        return process+" : "+guests+" guests --> "+tables+ " tables calculated with max "+max_capacity+" people per table"
+
+
+    def plot(self):
+        fig = px.scatter(self.dtf, x="x", y="y", color="category", hover_name="name", facet_col="table", facet_col_wrap=3,
+                         hover_data={"x":False, "y":False, "category":True, "avoid":True, "size":False, "table":False},
+                         size="size")
+
+        fig.add_shape(type="circle", opacity=0.1, fillcolor="black", col="all", row="all", exclude_empty_subplots=True,
+                      x0=self.dtf["x"].min(), y0=self.dtf["y"].min(), x1=self.dtf["x"].max(), y1=self.dtf["y"].max())
+
+        fig.update_layout(plot_bgcolor='white', legend={"bordercolor":"black", "borderwidth":1, "orientation":"h"})
+        fig.update_yaxes(visible=False)
+        fig.update_xaxes(visible=False)
+        return fig
